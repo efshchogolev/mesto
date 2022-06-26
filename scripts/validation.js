be39a8f9f8a5e8de90cd75d52da8f66191a7dbf4
Efim Shchegolev <efimblashck@yandex.ru>
2022-06-27 01:00:20 +0300

    feat: сделать рабочую валидацию попапов

---
+++ scripts/validation.js
@@ -0,0 +1,63 @@
+
+const activateError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`)
+  inputElement.classList.add('popup__input_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__input-error_active');
+};
+
+const disableError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`)
+  inputElement.classList.remove('popup__input_error')
+  errorElement.classList.remove('popup__input-error_active');
+  errorElement.textContent = '';
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    activateError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    disableError(formElement, inputElement);
+  }
+};
+
+const toggleButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.disabled = true;
+  }
+  else {
+    buttonElement.disabled = false;
+  }
+}
+
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+  const buttonElement = formElement.querySelector('.popup__submit-button');
+  toggleButtonState(inputList, buttonElement);
+  inputList.forEach ((inputElement) => {
+    inputElement.addEventListener('input', function (){
+      checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    })
+  })
+}
+
+function enableValidation() {
+  const formList = Array.from(document.querySelectorAll('.popup__form'))
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit',function (evt) {
+      evt.preventDefault();
+    })
+     setEventListeners(formElement);
+  })
+}
+
+enableValidation();
+
+function hasInvalidInput(inputList) {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  })
+}
+
