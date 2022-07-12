72fc81b3948798e6b20680a828f2efa147bafdf4
Efim Shchegolev <efimblashck@yandex.ru>
2022-07-12 15:27:22 +0300

    fix: добавить испарвления класса Card

--- scripts/Card.js
+++ scripts/Card.js
@@ -12,43 +12,41 @@ class Card {
       .content.children[0].cloneNode(true);
   }
 
-  _addEvemtListeners() {
+  _addEventListeners() {
     this._view
       .querySelector(this._config.cardDeleteButton)
       .addEventListener("click", (evt) => {
         this._deleteCard(evt);
       });
-    this._view
-      .querySelector(this._config.cardLikeButton)
-      .addEventListener("click", (evt) => {
-        this._likeCard(evt);
-      });
-    this._view
-      .querySelector(this._config.cardImage)
-      .addEventListener("click", () => {
-        this._openPlacePopup(this._name, this._image);
-      });
+    this._placeImage.addEventListener("click", () => {
+      this._openPlacePopup(this._name, this._image);
+    });
+    this._buttonLike.addEventListener("click", () => {
+      this._likeCard();
+    });
   }
 
   _deleteCard(evt) {
     evt.preventDefault();
     this._view.remove();
+    this._view = null;
   }
 
-  _likeCard(evt) {
-    evt.target.classList.toggle("place__like-button_active");
+  _likeCard() {
+    this._buttonLike.classList.toggle("place__like-button_active");
   }
 
   render(cardsList) {
     this._view = this._getTemplate();
-    const placeName = this._view.querySelector(this._config.cardName);
-    placeName.textContent = this._name;
-    const placeImage = this._view.querySelector(this._config.cardImage);
-    placeImage.src = this._image;
-    placeImage.alt = this._name;
+    this._placeName = this._view.querySelector(this._config.cardName);
+    this._placeName.textContent = this._name;
+    this._placeImage = this._view.querySelector(this._config.cardImage);
+    this._placeImage.src = this._image;
+    this._placeImage.alt = this._name;
     cardsList.append(this._view);
+    this._buttonLike = this._view.querySelector(this._config.cardLikeButton);
 
-    this._addEvemtListeners();
+    this._addEventListeners();
   }
 }
 
