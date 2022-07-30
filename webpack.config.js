44b8e1edad9fbc244fe2c451148eb204377a98e2
Efim Shchegolev <efimblashck@yandex.ru>
2022-07-30 17:31:26 +0300

    refact: переделать файловую структуру

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: {
-    build: "./src/scripts/index.js",
+    build: "./src/pages/index.js",
   },
   output: {
     filename: "bundle.js",
